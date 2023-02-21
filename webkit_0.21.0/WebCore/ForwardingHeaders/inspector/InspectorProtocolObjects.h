3db528b86857b48a3d012be176f7e784573a8cad
Smitty <me@smitop.com>
2023-02-20 20:50:24 -0500

    16.0.0

--- webkit_0.21.0/WebCore/ForwardingHeaders/inspector/InspectorProtocolObjects.h
+++
@@ -1,8 +0,0 @@
-#ifndef WebCore_FWD_InspectorProtocolObjects_h
-#define WebCore_FWD_InspectorProtocolObjects_h
-#if !PLATFORM(WKC)
-#include <JavaScriptCore/InspectorProtocolObjects.h>
-#else
-#include <InspectorProtocolObjects.h>
-#endif
-#endif
--- webkit_2.21.5/WebCore/PAL/pal/PlatformWinCairo.cmake
+++
@@ -1,6 +0,0 @@
-list(APPEND PAL_PUBLIC_HEADERS
-)
-
-list(APPEND PAL_SOURCES
-    crypto/openssl/CryptoDigestOpenSSL.cpp
-)
